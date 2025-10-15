4e23243630f7021077c21b4f7e1c2a60676e3c32
Wilfred Fung <185342426+bfng72@users.noreply.github.com>
2025-10-15 20:12:24 +0100

    Update set-setup.sh to download SST packages
    Added commands to download and extract SST core and elements.

---
+++ set-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/sstsimulator/sst-core/releases/download/v15.0.0_Final/sstcore-15.0.0.tar.gz
+wget https://github.com/sstsimulator/sst-elements/releases/download/v15.0.0_Final/sstelements-15.0.0.tar.gz
+
+tar xzf sstcore-15.0.0.tar.gz
+tar xzf sstelements-15.0.0.tar.gz
